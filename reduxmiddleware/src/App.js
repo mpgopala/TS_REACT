6277d51b6d0b06a6f06bec457461be0b6d8669cf
Banuprakash <banuprakashc@yahoo.co.in>
2022-02-11 17:08:37 +0530

    day 8

--- reduxmiddleware/src/App.js
+++ reduxmiddleware/src/App.js
@@ -3,9 +3,10 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import {userReducer} from './redux/Reducers';
 
 import thunk from 'redux-thunk';
+import Users from './User';
 
 let store = createStore(userReducer, 
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__());
+  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 function App() {
   return <Provider store={store}>
